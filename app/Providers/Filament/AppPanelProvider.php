ca331d08e22655e38c564c35a0aec951022079d4
Geoffery Usir <usirgeoffery@travimobile.com>
2025-12-24 08:38:49 +0100

    done with login seperation

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers\Filament;
 
 use Filament\Panel;
-use App\Enums\UserRole;
 use Filament\PanelProvider;
 use Filament\Pages\Dashboard;
 use Filament\Support\Colors\Color;
@@ -19,6 +18,7 @@ use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use DiogoGPinto\AuthUIEnhancer\AuthUIEnhancerPlugin;
 
 class AppPanelProvider extends PanelProvider
 {
@@ -30,9 +30,16 @@ class AppPanelProvider extends PanelProvider
             ->path('app')
             ->login()
             ->authMiddleware(['auth'])
+
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Emerald,
             ])
+            ->viteTheme('resources/css/filament/app/theme.css')
+            ->brandName('GreenPea')
+            ->brandLogo(asset('img/greenpea-favicon.png'))
+            ->brandLogoHeight('3.5rem')
+            ->favicon(asset('img/greenpea-favicon.png'))
+            ->font('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
@@ -40,8 +47,20 @@ class AppPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                // AccountWidget::class,
+                // FilamentInfoWidget::class,
+            ])
+            // ->topbar(false)
+            ->plugins([
+                AuthUIEnhancerPlugin::make()
+                    ->formPanelPosition('right')
+                    ->mobileFormPanelPosition('bottom')
+                    ->formPanelWidth('40%')
+                    // ->formPanelBackgroundColor(Color::White, '300')
+                    // ->formPanelBackgroundColor(Color::hex('#ffffff'))
+                    ->emptyPanelBackgroundImageUrl('https://res.cloudinary.com/dney6qnzd/image/upload/v1722059138/seminar-bg_tyc3gc.jpg')
+                    ->emptyPanelBackgroundImageOpacity('90%')
+                    ->emptyPanelBackgroundColor(Color::Green, '300')
             ])
             ->middleware([
                 EncryptCookies::class,
