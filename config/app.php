3ed9f2fb8f6c957edf06b98c7e87bdf60a8dab7a
Geoffery Usir <usirgeoffery@travimobile.com>
2026-02-13 03:50:02 +0100

    optimize url

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'admin_url' => env('ADMIN_URL', '/admin'),
+    'admin_url' => env('ADMIN_URL'),
 
     /*
     |--------------------------------------------------------------------------
